75dc7ac68f03523fc456923ee8b07896e8219884
!Mensch <assassinste@gmail.com>
2022-08-17 15:40:31 +0800

    feat: adding /about page and its route

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,11 +1,14 @@
 import React, { lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
+
 import Main from './views/pages/Home';
+import AboutPage from './views/pages/AboutPage';
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={Main} />
+      <Route exact path="/about" component={AboutPage} />
     </Switch>
   );
 };
