a99098a1daa7a4059ee9cf5bdfd0fae74284832e
!Mensch <assassinste@gmail.com>
2022-08-17 16:45:13 +0800

    dev: remove material button demonstration code

--- src/app/views/pages/Home.tsx
+++ src/app/views/pages/Home.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import BasicButtons from 'app/components/material-buttons';
 
 const Main = () => {
   return (
     <div>
       <h1>Homepage for React</h1>
-      <BasicButtons />
     </div>
   );
 };
