9452a2691895a4afd92af6cbbc07d9180ae52b03
!Mensch <assassinste@gmail.com>
2022-08-18 22:25:46 +0800

    feat: adding /not-found page and its route

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import Main from './views/pages/Home';
 import AboutPage from './views/pages/AboutPage';
+import NotFoundPage from './views/pages/NotFoundPage';
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={Main} />
       <Route exact path="/about" component={AboutPage} />
+      <Route exact path="/not-found" component={NotFoundPage} />
     </Switch>
   );
 };
