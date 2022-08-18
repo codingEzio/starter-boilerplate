9f40dc42c7615f2b20766324ac1e39b6d2529d4d
!Mensch <assassinste@gmail.com>
2022-08-18 22:29:07 +0800

    feat: redirect non-existing routes to /not-found

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Main from './views/pages/Home';
 import AboutPage from './views/pages/AboutPage';
@@ -11,6 +11,8 @@ const Routes = () => {
       <Route exact path="/" component={Main} />
       <Route exact path="/about" component={AboutPage} />
       <Route exact path="/not-found" component={NotFoundPage} />
+
+      <Redirect from={'*'} to={'/not-found'} exact />
     </Switch>
   );
 };
