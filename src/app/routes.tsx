c6d9c19cb874c6e2bf8541469ee5b2c45b2452fc
!Mensch <assassinste@gmail.com>
2022-08-21 18:35:24 +0800

    feat: prep routes for sidebar navigation

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -6,6 +6,9 @@ import { Container } from '@material-ui/core';
 import Main from './views/pages/Home';
 import AboutPage from './views/pages/AboutPage';
 import NotFoundPage from './views/pages/NotFoundPage';
+import Dashboard from './layouts/dashboard-layout';
+import DashboardDefaultContent from './views/dashboard/dashboard-default-content';
+import SettingsAndPrivacy from './views/dashboard/settings-and-privacy';
 
 const Routes = () => {
   return (
@@ -14,6 +17,27 @@ const Routes = () => {
         <Route exact path="/" component={Main} />
         <Route exact path="/about" component={AboutPage} />
         <Route exact path="/not-found" component={NotFoundPage} />
+
+        <Route
+          path={'/dashboard'}
+          render={({ match: { path } }) => (
+            <Dashboard>
+              <Switch>
+                <Route
+                  exact
+                  path={path + '/'}
+                  component={DashboardDefaultContent}
+                />
+
+                <Route
+                  exact
+                  path={path + '/settings-and-privacy'}
+                  component={SettingsAndPrivacy}
+                />
+              </Switch>
+            </Dashboard>
+          )}
+        />
 
         <Redirect from={'*'} to={'/not-found'} exact />
       </Container>
