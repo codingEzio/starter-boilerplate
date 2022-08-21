fd172516b555b1608abe775fa318b334fc7bbf34
!Mensch <assassinste@gmail.com>
2022-08-21 11:47:44 +0800

    dev: templates for dashboard

---
+++ src/app/views/dashboard/dashboard-default-content.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashboardDefaultContent = () => {
+  return (
+    <div>
+      <h1>DashboardDefaultContent</h1>
+    </div>
+  );
+};
+
+export default DashboardDefaultContent;
