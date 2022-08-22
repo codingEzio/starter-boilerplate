f5b9072b11ef50d6a6a59650e6b73c2ae1165166
!Mensch <assassinste@gmail.com>
2022-08-22 11:59:17 +0800

    dev: Fetching data from our own backend API /sales

--- src/app/views/dashboard/dashboard-default-content.tsx
+++ src/app/views/dashboard/dashboard-default-content.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getSalesAxios } from 'services/saleService';
+
+const fetchSales = async () => {
+  const data = await getSalesAxios();
+
+  console.log(data);
+};
 
 const DashboardDefaultContent = () => {
+  useEffect(() => {
+    fetchSales();
+  }, []);
+
   return (
     <div>
       <h1>DashboardDefaultContent</h1>
